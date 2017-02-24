f5745eea445ada408d5b4c722902985f836b5623
darthMilash <dani4eg92@gmail.com>
2017-02-24 11:51:16 +0200

    add RootWindow TaskWindow MainController update MainApp pom.xml

--- src/main/java/com/darth/milash/MainApp.java
+++ src/main/java/com/darth/milash/MainApp.java
@@ -1,13 +1,75 @@
 package com.darth.milash;
 
-/**
- * Hello world!
- *
- */
-public class MainApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("TaskMan");
+
+        initRootWindowt();
+
+        initTaskWindow();
+    }
+
+    /**
+     * Инициализирует корневой макет.
+     */
+    public void initRootWindowt() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fxml/RootWindow.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Показывает в корневом макете сведения об адресатах.
+     */
+    public void initTaskWindow() {
+        try {
+            // Загружаем сведения об адресатах.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fxml/TaskWindow.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Возвращает главную сцену.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/com/darth/milash/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.darth.milash.controller;
+
+/**
+ * Created by darthMilash on 24.02.2017.
+ */
+public class MainController {
+}
